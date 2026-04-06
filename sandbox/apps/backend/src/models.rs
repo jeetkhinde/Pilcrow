5ce3f9dd73021909dc7d3c5c892fced5d33203f0
Jagjeet Singh <jeetkhinde@gmail.com>
2026-04-06 10:49:47 -0400

    removed sandbox

--- sandbox/apps/backend/src/models.rs
+++
@@ -1,8 +0,0 @@
-use serde::{Deserialize, Serialize};
-
-#[derive(Debug, Clone, Serialize, Deserialize, PartialEq, Eq)]
-pub struct Todo {
-    pub id: i64,
-    pub title: String,
-    pub done: bool,
-}
--- sandbox/apps/web/build.rs
+++
@@ -1,7 +0,0 @@
-fn main() {
-    if let Err(err) = routekit::compile_current_crate_sources() {
-        eprintln!("Pilcrow template compile error:");
-        eprintln!("{err}");
-        std::process::exit(1);
-    }
-}
